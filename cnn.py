e09aefd7f4ad908d761f8232f56c37aba1eca867
Stephen Blum <blum.stephen@gmail.com>
2026-03-16 14:09:17 -0700

    cnn mostly ready, though we have a bug ready

--- cnn.py
+++ cnn.py
@@ -17,6 +17,32 @@ class LeNet():
 
     def forward(self, input):
         out = F.max_pool2d(F.relu(self.conv1(input)), (2,2))
+        out = F.max_pool2d(F.relu(self.conv2(out)), 2)
+        out = out.view(-1, self.num_flat_features(out))
+
+        #print
+        ## BROKEN HERE< FIX!!!
+        #out = F.relu(self.linear1(out))
+        #out = F.relu(self.linear2(out))
+        #out = self.linear2(out)
+
+        return out
+
+    def num_flat_features(self, x):
+        size = x.size()[1:] ## excluding batch size
+        features = 1
+
+        for s in size:
+            features *= s
+
+        return features
+
+r = torch.rand(1,25,25)
+lenet = LeNet()
+out = lenet.forward(r)
+print(out)
+
+
 
 
 
